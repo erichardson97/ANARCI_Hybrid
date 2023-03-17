c41e1bdd4babcd6c5ae9c1673de5c1c8fe496e9e
erichardson97 <erichardson@lji.org>
2023-03-17 10:01:36 -0700

    Separate setup.py and data parts

---
+++ build_pipeline/download.py
@@ -0,0 +1,29 @@
+import importlib, subprocess, os, shutil
+
+ANARCI_LOC = importlib.util.find_spec("anarci")
+if ANARCI_LOC is None:
+    raise Exception("ANARCI module not found - maybe you deleted it or something you freakazoid")
+    sys.exit(1)
+else:
+    ANARCI_LOC = ANARCI_LOC.submodule_search_locations[0]
+
+try:
+    #shutil.rmtree("curated_alignments/")
+    shutil.rmtree("muscle_alignments/")
+    shutil.rmtree("HMMs/")
+    shutil.rmtree("IMGT_sequence_files/")
+    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
+except OSError:
+    pass
+
+print('Downloading germlines from IMGT and building HMMs...')
+proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+o, e = proc.communicate()
+
+print(o.decode())
+print(e.decode())
+
+shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
+if os.path.exists(os.path.join(ANARCI_LOC, "dat/HMMs/")):
+  shutil.rmtree(os.path.join(ANARCI_LOC, "dat/HMMs/"))
+shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
--- setup.py
+++ setup.py
@@ -33,38 +33,6 @@ setup(name='anarci',
 import sys
 if sys.argv[1] != "install":
     sys.exit(0)
+subprocess.call('python build_pipeline/download.py')
 
-try:
-    module_dir_list = [os.path.join(os.getcwd(), 'lib', 'python')]
-    print(module_dir_list)
-    ANARCI_LOC = importlib.machinery.PathFinder().find_spec("anarci", module_dir_list)
-    if ANARCI_LOC is None:
-        raise Exception("ANARCI module not found - maybe you deleted it or something you freakazoid")
-    else:
-        ANARCI_LOC = ANARCI_LOC.submodule_search_locations[0]
-except Exception as e:
-    sys.stderr.write(e.message)
-    sys.exit(1)
 
-os.chdir("build_pipeline")
-
-try:
-    #shutil.rmtree("curated_alignments/")
-    shutil.rmtree("muscle_alignments/")
-    shutil.rmtree("HMMs/")
-    shutil.rmtree("IMGT_sequence_files/")
-    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
-except OSError:
-    pass
-
-print('Downloading germlines from IMGT and building HMMs...')
-proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-o, e = proc.communicate()
-
-print(o.decode())
-print(e.decode())
-
-shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-if os.path.exists(os.path.join(ANARCI_LOC, "dat/HMMs/")):
-  shutil.rmtree(os.path.join(ANARCI_LOC, "dat/HMMs/"))
-shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
