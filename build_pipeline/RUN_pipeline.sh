ae83f704a25d1cb8d9300d5913f14a2754879302
Eve Richardson <66529930+erichardson97@users.noreply.github.com>
2023-03-22 18:20:08 -0700

    Update RUN_pipeline.sh

--- build_pipeline/RUN_pipeline.sh
+++ build_pipeline/RUN_pipeline.sh
@@ -28,10 +28,10 @@ if [ ! -d $DIR/HMMs ]; then
   mkdir -p $DIR/HMMs
 fi
 
-hmmbuild --hand $DIR/HMMs/ALL.hmm $DIR/curated_alignments/ALL.stockholm
-#hmmbuild --hand $DIR/HMMs/ALL_AND_C.hmm $DIR/curated_alignments/ALL_AND_C.stockholm
+#hmmbuild --hand $DIR/HMMs/ALL.hmm $DIR/curated_alignments/ALL.stockholm
+hmmbuild --hand $DIR/HMMs/ALL_AND_C.hmm $DIR/curated_alignments/ALL_AND_C.stockholm
 
 # Turn the output HMMs file into a binary form. This is required for hmmscan that is used in ARNACI.
-hmmpress -f $DIR/HMMs/ALL.hmm 
-#hmmpress -f $DIR/HMMs/ALL_AND_C.hmm
+#hmmpress -f $DIR/HMMs/ALL.hmm 
+hmmpress -f $DIR/HMMs/ALL_AND_C.hmm
 
